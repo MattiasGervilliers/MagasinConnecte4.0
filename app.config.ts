23e68c8d18e6deb7a67e623bd1b05ca69c493ae1
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2024-05-17 09:54:15 +0200

    feat: setup ui button

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,27 @@
+/*
+  button color: primary-500
+  button color on dark theme: primary-600
+*/
 export default defineAppConfig({
   ui: {
-    primary: '_primary',
-    gray: '_secondary',
-  }
-})
+    primary: "_primary",
+    gray: "_secondary",
+
+    button: {
+      color: {
+        prim: {
+          solid:
+            "text-white dark:text-black bg-primary-600 hover:bg-primary-700 disabled:bg-primary-200 dark:bg-primary-600 dark:hover:bg-primary-700 dark:disabled:bg-primary-900 transition-all",
+          outline:
+            "text-primary dark:text-primary-600 bg-white hover:bg-primary-600 hover:text-white hover:ring-0 ring-1 ring-inset ring-current disabled:bg-primary-200 dark:bg-white dark:hover:bg-primary-600 dark:hover:text-white dark:disabled:bg-primary-900 transition-all",
+          link: "text-primary dark:text-primary-600 hover:underline transition-all",
+        },
+      },
+      default: {
+        size: "sm",
+        variant: "solid",
+        color: "prim",
+      },
+    },
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
+<script setup></script>
+
 <template>
   <div>
     <h1>Magasin connecté 4.0</h1>
+    <UButton variant="solid">solid</UButton>
+    <UButton variant="solid" disabled>solid</UButton>
+
+    <UButton variant="outline">outline</UButton>
+    <UButton variant="soft">soft</UButton>
   </div>
 </template>
