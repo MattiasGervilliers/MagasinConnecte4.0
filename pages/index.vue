62977064bb22901160a89ca599ea813222d05e17
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2024-05-17 09:59:38 +0200

    refactor: remove useless buttons

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,5 @@
 <template>
   <div>
     <h1>Magasin connecté 4.0</h1>
-    <UButton variant="solid">solid</UButton>
-    <UButton variant="solid" disabled>solid</UButton>
-
-    <UButton variant="outline">outline</UButton>
-    <UButton variant="soft">soft</UButton>
   </div>
 </template>
