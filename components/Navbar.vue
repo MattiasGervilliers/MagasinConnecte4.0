be33c17a9ca8d7e959c544a3f84914d59bedd69d
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2024-05-18 19:16:37 +0200

    feat: starting development of navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,43 @@
-<script setup></script>
+<script setup lang="ts">
+const isMenuOpen = useState("isMenuOpen", () => false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+const links = [
+  {
+    name: "Technologies",
+    path: "/technologies",
+  },
+  {
+    name: "Photovoltaïque",
+    path: "/photovoltaique",
+  },
+  {
+    name: "Epicerie-solidaire",
+    path: "/epicerie-solidaire",
+  },
+];
+</script>
 
 <template>
-  <nav>
-    <p>Navbar</p>
+  <nav class="navbar">
+    <h1>Magasin Connecté 4.0</h1>
+
+    <div class="navbar-menu-container">
+      <ul class="menu" v-if="isMenuOpen">
+        <li v-for="link in links" :key="link.name">
+          <UButton :to="link.path" variant="link">{{ link.name }}</UButton>
+        </li>
+      </ul>
+
+      <UIcon @click="toggleMenu" v-if="!isMenuOpen" name="i-heroicons-bars-3" />
+      <UIcon @click="toggleMenu" v-if="isMenuOpen" name="i-heroicons-x-mark" />
+    </div>
   </nav>
 </template>
+
+<style>
+@import url("~/assets/css/navbar.css");
+</style>
---
+++ pages/epicerie-solidaire.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const links = [
+  {
+    label: "Magasin Connecté 4.0",
+    to: "/",
+  },
+  {
+    label: "Epicerie Solidaire",
+  },
+];
+</script>
+
+<template>
+  <UBreadcrumb :links="links">
+    <template #divider>
+      <span class="w-8 h-1 rounded-full bg-neutral-700 dark:bg-neutral-300" />
+    </template>
+  </UBreadcrumb>
+</template>
