1079d4f6835790f5283fafe03660e911b0616734
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2024-05-22 09:35:19 +0200

    feat: starting development of homepage

---
+++ components/CardNavigation.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{image: ReturnType<typeof defineComponent>, isReversed?: boolean}>()
+</script>
+
+<template>
+  <div class="card-navigation">
+    <component :is="props.image" v-if="props.image" />
+  </div>
+</template>
+
+<style>
+.card-navigation {
+  width: 200px;
+  height: 200px;
+  border: 1px solid theme('colors._primary.500');
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
-<template></template>
+<template>
+  <NuxtImg class="background" src="background.jpg" />
+
+  <Card />
+  <Card is-reversed />
+
+</template>
+
+<style>
+.background {
+  width: 100%;
+  height: calc(100vh - 70px);
+  object-fit: cover;
+}
+</style>
