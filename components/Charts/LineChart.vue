84a15c8c0db2cf6703ff936c51e4749ab353ee4f
HisokaBluee <mattias.gervilliers@etu.univ-amu.fr>
2024-05-23 10:32:44 +0200

    feat(chart): Add LineChart support and an example chart

---
+++ components/Charts/LineChart.vue
@@ -0,0 +1,44 @@
+<template>
+  <Line
+    chart-id="test-chart"
+    :data="data"
+    :options="options"
+    style="height: 100%"
+  />
+</template>
+
+<script>
+import { Line } from "vue-chartjs";
+import { defaultChartOptions, defaultRegister } from "~/components/Charts/defaultChart.js";
+import { Chart as ChartJS } from "chart.js";
+
+ChartJS.register(...defaultRegister);
+
+export default {
+  components: {
+    Line,
+  },
+  data() {
+    return {
+      data: this.chartData,
+    };
+  },
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    chartOptions: {
+      type: Object,
+    },
+  },
+  computed: {
+    options() {
+      return {
+        ...defaultChartOptions,
+        ...this.chartOptions
+      };
+    },
+  },
+};
+</script>
