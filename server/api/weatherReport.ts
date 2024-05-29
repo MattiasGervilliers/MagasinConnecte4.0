4e4aa991a68dcd1d26b772f74021b67f1d03b63a
Guillaume Kusiak <91054044+GuillaumeKusiak@users.noreply.github.com>
2024-05-29 11:56:30 +0200

    feat: new weather api (#29) (#34)
    * feat: now using weatherBit instead of meteoFrance
    
    * feat: calculate the theoretical production
    
    * refactor: removing unused import
    
    * refactor: removing parser file because marius is always right
    
    * refactor: cleaner code
    
    ---------
    
    Co-authored-by: Alessioplt <93313802+Alessioplt@users.noreply.github.com>
    Co-authored-by: AlessioPulit <apulitano@wiser.com>

--- server/api/weatherReport.ts
+++ server/api/weatherReport.ts
@@ -1,16 +1,17 @@
 import { useFetchWeatherReport } from "~/composables/useFetchWeatherReport";
 
 type Query = {
-  departmentId: string;
+  frequency: string;
   beginningDate: string;
   endDate: string;
+  nominalPower: string;
+  performanceRatio: string;
 };
 
 export default defineEventHandler(async (event) => {
-  const { departmentId, beginningDate, endDate} = getQuery<Query>(event);
-
+  const { frequency, beginningDate, endDate, nominalPower, performanceRatio} = getQuery<Query>(event);
   const config = useRuntimeConfig();
 
   const apiKey = config.apiKey;
-  return await useFetchWeatherReport({queryParams: {departmentId, beginningDate, endDate}, apiKey});
+  return await useFetchWeatherReport({queryParams: {frequency, beginningDate, endDate}, apiKey, solarPanelInfo: {nominalPower, performanceRatio}});
 });
