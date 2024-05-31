1536400a711379f78bba380fee3603cf9bc77e04
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2024-05-31 11:11:39 +0200

    feat: finish login validation with pwd and email

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,42 +1,53 @@
-import { createError, eventHandler, readBody } from 'h3'
-import { z } from 'zod'
-import { sign } from 'jsonwebtoken'
+import { createError, eventHandler, readBody } from "h3";
+import { z } from "zod";
+import { sign } from "jsonwebtoken";
+import sha256 from "crypto-js/sha256";
+import { UserRepository } from "~/server/repository/UserRepository";
 
-const config = useRuntimeConfig()
+const config = useRuntimeConfig();
 
 const SECRET = config.jwtSecret;
 
+type LoginData = {
+  username: string;
+  password: string;
+};
+
 export default eventHandler(async (event) => {
+  const loginData: LoginData = await readBody(event);
+  const pwd: string =
+    (await UserRepository.getUserByEmail(loginData.username))?.password || "";
+
   const result = z
-    .object({ username: z.string().min(1), password: z.literal('hunter2') }) //TODO: Load real password and check it
-    .safeParse(await readBody(event))
+    .object({ username: z.string().min(1), password: z.literal(pwd) })
+    .safeParse(loginData);
   if (!result.success) {
     throw createError({
       statusCode: 403,
-    })
+    });
   }
 
-  const expiresIn = 15
+  const expiresIn = 15;
 
-  const { username } = result.data
+  const { username } = result.data;
 
   const user = {
     username,
-    picture: 'https://github.com/nuxt.png',
-    name: 'User ' + username
-  }
+    picture: "https://github.com/nuxt.png",
+    name: "User " + username,
+  };
 
-  const accessToken = sign({ ...user, scope: ['test', 'user'] }, SECRET, {
-    expiresIn
-  })
-  const refreshToken = sign({ ...user, scope: ['test', 'user'] }, SECRET, {
-    expiresIn: 60 * 60 * 24
-  })
+  const accessToken = sign({ ...user, scope: ["test", "user"] }, SECRET, {
+    expiresIn,
+  });
+  const refreshToken = sign({ ...user, scope: ["test", "user"] }, SECRET, {
+    expiresIn: 60 * 60 * 24,
+  });
 
   return {
     token: {
       accessToken,
-      refreshToken
-    }
-  }
-})
+      refreshToken,
+    },
+  };
+});
