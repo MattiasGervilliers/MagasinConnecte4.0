eac345a4861027fa30c8e1d1901f82844a4f9439
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2024-06-03 13:54:06 +0200

    refactor: remove useless code

--- constants/defaultValues.ts
+++ constants/defaultValues.ts
@@ -1,38 +1,4 @@
-import type { Day, Shop } from "~/models/shop";
-
-export const SHOP_1: Shop = {
-  name: "Shop 1",
-  description: "This is a shop",
-  address: "123 Fake St",
-  image: "https://via.placeholder.com/150",
-  lat: 51.5074,
-  lng: 0.1278,
-  currentWeek: {
-    number: 1,
-    days: [],
-  },
-  nextWeek: {
-    number: 2,
-    days: [],
-  },
-};
-
-export const SHOP_2: Shop = {
-  name: "Shop 2",
-  description: "This is another shop",
-  address: "456 Fake St",
-  image: "https://via.placeholder.com/150",
-  lat: 78.5074,
-  lng: 0.1278,
-  currentWeek: {
-    number: 1,
-    days: [],
-  },
-  nextWeek: {
-    number: 2,
-    days: [],
-  },
-};
+import type { Day } from "~/models/shop";
 
 export const DEFAULT_DAYS: Omit<Day, "day"> = {
   isOpen: false,
@@ -42,5 +8,3 @@ export const DEFAULT_DAYS: Omit<Day, "day"> = {
   afternoonStart: "",
   afternoonEnd: "",
 };
-
-export const SHOPS: Shop[] = [SHOP_1, SHOP_2];
