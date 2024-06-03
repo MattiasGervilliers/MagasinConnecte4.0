775532bde72b6e330fccfb67f20253186761b7df
Guillaume Kusiak <91054044+GuillaumeKusiak@users.noreply.github.com>
2024-06-03 14:13:25 +0200

    feat: add pwd management (#52)

--- server/repository/UserRepository.ts
+++ server/repository/UserRepository.ts
@@ -1,22 +1,35 @@
-import { User } from '~/models/user'
-import { JsonConnector } from '../connector/JsonConnector'
+import { User } from "~/models/user";
+import { JsonConnector } from "../connector/JsonConnector";
 
 export class UserRepository {
-  static filePath: string = process.cwd() + '/data/user.json'
+  static filePath: string = process.cwd() + "/data/user.json";
 
   // TODO: Implement the method in front
   public static async saveUsers(users: User[]): Promise<User[]> {
-    await JsonConnector.saveData(users, this.filePath)
+    await JsonConnector.saveData(users, this.filePath);
 
-    return (await JsonConnector.getData(this.filePath)) as User[]
+    return (await JsonConnector.getData(this.filePath)) as User[];
   }
 
   public static async getUsers(): Promise<User[]> {
-    return (await JsonConnector.getData(this.filePath)) as User[]
+    return (await JsonConnector.getData(this.filePath)) as User[];
+  }
+
+  public static async saveUserByEmail(
+    email: string,
+    user: User,
+  ): Promise<User> {
+    const users = await this.getUsers();
+    const index = users.findIndex((user: User) => user.email === email);
+    users[index] = user;
+
+    return (await this.saveUsers(users)).find(
+      (user: User) => user.email === email,
+    ) as User;
   }
 
   public static async getUserByEmail(email: string): Promise<User | undefined> {
-    const users = await this.getUsers()
-    return users.find((user) => user.email === email)
+    const users = await this.getUsers();
+    return users.find((user: User) => user.email === email);
   }
 }
