8415e236744b723f2058beacc8004d042b3256db
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2024-06-04 16:32:03 +0200

    feat: add global wrapper

---
+++ components/GlobalWrapper.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { withFullscreen } = defineProps<{ withFullscreen?: boolean }>();
+</script>
+
+<template>
+  <div
+    :class="{
+      'global-wrapper': true,
+      fullscreen: withFullscreen,
+    }"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+@import url("~/assets/css/globalWrapper.css");
+</style>
