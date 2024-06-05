003001e7061a53cb971da9b723c137a5f873dc42
Marius <marius.pistoresi@etu.univ-amu.fr>
2024-06-05 08:39:06 +0200

    feat: add contact page with mailto action

--- components/BlurBackground.vue
+++ components/BlurBackground.vue
@@ -17,8 +17,8 @@ const props = defineProps<{
 <style scoped>
 .blur-bg {
   position: absolute;
-  top: 0;
+  top: 50%;
   left: 50%;
-  transform: translate(-50%, 50%);
+  transform: translate(-50%, -50%);
 }
 </style>
