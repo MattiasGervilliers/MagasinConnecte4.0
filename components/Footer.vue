a4aa0d711c3014fd39a534717d187ed8b118ca23
Guillaume Kusiak <91054044+GuillaumeKusiak@users.noreply.github.com>
2024-06-05 09:24:22 +0200

    fix: nuxt img and responsive (#79)

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,24 +8,44 @@ onMounted(() => {
 
 <template>
   <footer class="footer">
-    <div class="container py-5">
+    <div class="container py-5 justify-center lg:justify-between">
       <div class="logo">
-        <NuxtImg :src="isDarkTheme ? '/Logo_AMU_BLANC.png' : '/LOGO_AMU.png'" alt="Logo AMU" class="w-fit" />
+        <NuxtImg
+          :src="isDarkTheme ? 'Logo_AMU_BLANC.png' : 'LOGO_AMU.png'"
+          alt="Logo AMU"
+          class="w-fit"
+        />
       </div>
 
       <div>
-        <h2 class="text-xl font-bold text-_primary-700 dark:text-white">Liens rapides</h2>
+        <h2 class="text-xl font-bold text-_primary-700 dark:text-white">
+          Liens rapides
+        </h2>
         <ul class="footer-menu">
-          <li><NuxtLink to="technologies" class="hover:text-blue-800">Technologies</NuxtLink></li>
-          <li><NuxtLink to="photovoltaique" class="hover:text-blue-800">Photovoltaique</NuxtLink></li>
-          <li><NuxtLink to="epicerie-solidaire" class="hover:text-blue-800">Epicerie Solidaire</NuxtLink></li>
+          <li>
+            <NuxtLink to="technologies" class="hover:text-blue-800"
+              >Technologies</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink to="photovoltaique" class="hover:text-blue-800"
+              >Photovoltaique</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink to="epicerie-solidaire" class="hover:text-blue-800"
+              >Epicerie Solidaire</NuxtLink
+            >
+          </li>
         </ul>
       </div>
 
       <div>
-        <h2 class="text-xl font-bold text-_primary-700 dark:text-white">Magasin Connecté 4.0</h2>
+        <h2 class="text-xl font-bold text-_primary-700 dark:text-white">
+          Magasin Connecté 4.0
+        </h2>
         <p>
-          IUT d’Aix-Marseille – Site de l’étoile<br>
+          IUT d’Aix-Marseille – Site de l’étoile<br />
           142 traverse Charles Susini 13013 Marseille
         </p>
       </div>
