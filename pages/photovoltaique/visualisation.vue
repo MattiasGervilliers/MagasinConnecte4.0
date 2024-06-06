d71b2d9ffd9e567a06d3f16116d4e78facf8a0f3
Guillaume Kusiak <91054044+GuillaumeKusiak@users.noreply.github.com>
2024-06-06 14:43:17 +0200

    feat: pwa auto redirect (#123)

--- pages/photovoltaique/visualisation.vue
+++ pages/photovoltaique/visualisation.vue
@@ -3,28 +3,14 @@ import { definePageMeta } from "#imports";
 import Overview from "~/components/solarPanel/Overview.vue";
 
 definePageMeta({
-  auth: false
+  auth: false,
 });
-
-const links = [
-  {
-    label: "Magasin Connecté 4.0",
-    to: "/",
-  },
-  {
-    label: "Photovoltaïque",
-    to: "/photovoltaique",
-  },
-  {
-    label: "Visualisation",
-  },
-];
 </script>
 
 <template>
   <div class="visualization">
     <div class="overview">
-      <Overview/>
+      <Overview />
     </div>
     <div class="chart mt-5">
       <SolarPanelEnergyChart />
@@ -33,8 +19,8 @@ const links = [
 </template>
 
 <style scoped>
-  .visualization {
-    margin-top: 70px;
-    padding: 0 10%;
-  }
+.visualization {
+  margin-top: 70px;
+  padding: 0 10%;
+}
 </style>
